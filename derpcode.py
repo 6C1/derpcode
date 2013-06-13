d4594c29eca3746c770cf61f169ea4656443539c
6C1 <cooperstimson@gmail.com>
2013-06-12 22:38:51 -0400

    \'?\' get char first draft

--- derpcode.py
+++ derpcode.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import sys
+import sys, math
 from string import ascii_lowercase, ascii_uppercase
 
 version = "0.1.0"
+INIT_LEN = int(256)
 
 def bflip(n): return (1-n)
 
@@ -21,11 +22,19 @@ def fopen():
 class Tape():
     def __init__(self):
         self.printbuf=""
-        self.cells = [0 for x in xrange(256)]
+        self.cells = [0 for x in xrange(INIT_LEN)]
         self.pointer = 0
     def __repr__(self):
         return "\nPointer:    \t"+str(self.pointer)+"\nTape:    \t"+str(self.cells[1:])
     def get_pointer(self): return self.pointer + 1
+
+    def update(self):
+        if len(self.cells) - self.pointer < (math.ceil(len(self.cells)**0.25)):
+            for i in xrange(len(self.cells)):
+                self.cells.append(0)
+        for cell in self.cells:
+            if cell > 1:
+                print cell
 
     # derpcode commands
     def flip(self): self.cells[self.get_pointer()] = bflip(self.cells[self.get_pointer()]) 
@@ -36,15 +45,32 @@ class Tape():
             return False
         binstr = ""
         for i in xrange(8):
-            binstr+=(str(self.cells[self.get_pointer()+i]))
+            binstr+=(str(self.cells[self.get_pointer()+7-i]))
         self.printbuf+=str(chr(int(binstr,2)))
         return True
+    def gtc(self):
+        char = str(ord(sys.stdin.read(1)[0:1]))
+        char = bin(int(char))[2:].zfill(8)
+        print
+        for i in xrange(len(char)):
+            self.cells[self.pointer + i] = int(char[i])
+    def brh(self):
+        if self.cells[self.pointer] == 1:
+            string = ""
+            for i in xrange(8):
+                string += str(self.cells[self.pointer+i])
+            num = int(string,2)
+            print num
 
 def interpret(data):
     library = ["herp","derp","a-derp",".","?"]
     cmds = filter((lambda x : (x in library) or (x[0:-1] in library)), data.split())
     tape = Tape()
     for cmd in cmds:
+
+#        uncomment this for detailed stats    
+#        print cmd, "   \tPointer:",  tape.pointer, "\tBit:",  tape.cells[tape.pointer], "\tlen(tape):", len(tape.cells)
+
         if cmd == library[0]: tape.flip()
         elif cmd == library[1]: tape.inc()
         elif cmd == library[2]: tape.dec()
@@ -54,7 +80,10 @@ def interpret(data):
             if (tape.cells[1]==0 and not tape.prt()): break
         elif cmd == library[2]+library[3]+library[3]:
             for i in xrange(3): tape.dec()
+        elif cmd == library[4]: tape.gtc()
+        tape.update()
     print(tape.printbuf)
+
 
 def fherp(tape):
     tape.flip()
